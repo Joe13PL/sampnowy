6599c7e43d1503373730d86b0554b4ccbb7d27aa
JoePL <qsasukep@o2.pl>
2025-12-23 02:19:38 +0100

    modified:   config.json 	deleted:    crashdetect_logs.txt 	deleted:    gamemodes/main.amx 	deleted:    gamemodes/rp.amx 	deleted:    gamemodes/rp.pwn 	modified:   gamemodes/rp_openmp/admin/admin_commands.inc 	modified:   gamemodes/rp_openmp/core/defines.inc 	modified:   gamemodes/rp_openmp/gameplay/items.inc 	modified:   gamemodes/rp_openmp/gameplay/phone_system.inc 	modified:   gamemodes/rp_openmp/player/player_commands.inc 	modified:   gamemodes/rp_openmp/ui/dialog_handlers.inc 	modified:   gamemodes/scripts/compile_local.ps1

--- gamemodes/scripts/compile_local.ps1
+++ gamemodes/scripts/compile_local.ps1
@@ -1,4 +1,5 @@
 # Compile the gamemode locally using the bundled qawno/pawncc
-$Qawno = Join-Path $PSScriptRoot '..\qawno\pawncc.exe'
+# NOTE: qawno is at the workspace root, so jump up an extra level from gamemodes\scripts
+$Qawno = Join-Path $PSScriptRoot '..\..\qawno\pawncc.exe'
 $Main = Join-Path $PSScriptRoot '..\rp_openmp\main.pwn'
-& $Qawno $Main -i"$PSScriptRoot\..\qawno\include" -o"$PSScriptRoot\..\rp_openmp.amx" -v2
+& $Qawno $Main -i"$PSScriptRoot\..\..\qawno\include" -o"$PSScriptRoot\..\rp_openmp.amx" -v2
